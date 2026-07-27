446cdc9531778738566a68d4ce8e7b703a074610
Om-Prakash-Chaurasia <omchaurasia2024@gmail.com>
2026-07-27 11:46:39 +0530

    first commit

---
+++ FirstProgram.py
@@ -0,0 +1,41 @@
+print("Hello World!!!")
+
+# Here print() method is used to print output.
+
+# Printing in different line:
+print("My name is Om Prakash.")
+print("I am 26 years old.")
+
+# Printing in same line:
+print("My name is Om Prakash.", "I am 26 years old.")
+
+# Printing numbers in various formats:
+print(26)
+print(5 + 7)
+print(5 - 7)
+print(5 * 7)
+print(7 / 5)
+
+# Variables in Python:
+name = "Om Prakash"  # This variable has String value.
+age = 26  # This variable has Integer value.
+price = 25.99  # This variable has Float value.
+
+print(name)
+print(age)
+print(name, age, price)
+print("My name is", name, ".", "I am", age, "years old.")
+
+# Assigning one variable value to other variable:
+age2 = age
+print(age2)
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+# Data types in Python:
+# 1. Primitive Data types:
+# i. Integers
+# ii. String
+# iii. Float
