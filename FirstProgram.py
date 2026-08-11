3e517d73967feb15088b032b36e4ff368fff262a
Om-Prakash-Chaurasia <omchaurasia2024@gmail.com>
2026-08-11 20:29:06 +0530

    Lecture 4 is completed.

--- FirstProgram.py
+++
@@ -1,8 +0,0 @@
-student = {
-    "name": "Sonu Chaurasia",
-    "subjects": {"physics": 97, "chemistry": 98, "maths": 95},
-}
-
-print(student)
-student.update({"city": "Delhi", "age": 26})
-print(student)
