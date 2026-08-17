62f615a55e316d51f36a2a863fde06c4fe2bfffa
Om-Prakash-Chaurasia <omchaurasia2024@gmail.com>
2026-08-17 20:00:01 +0530

    Started Lecture 7: File Input / Output in Python.

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,12 +1,2 @@
-fruits = ["apple", "mango", "banana", "guava", "strawberry", "pineapple"]
-
-
-def printList(list, index=0):
-    if index == len(list):
-        return
-
-    print(list[index], end=" ")
-    printList(list, index + 1)
-
-
-printList(fruits, 0)
+f = open("sample.txt", "w")
+f.close()
