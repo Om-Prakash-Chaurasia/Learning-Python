8c3bd356e9cc638954515219eccb6691a0f93abb
Om-Prakash-Chaurasia <omchaurasia2024@gmail.com>
2026-09-05 17:49:31 +0530

    Lecture 7 is completed.

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,2 +1,19 @@
-f = open("sample.txt", "w")
-f.close()
+count = 0
+with open("practice.txt", "r") as f:
+    data = f.read()
+
+    num = ""
+    for i in range(len(data)):
+        if data[i] == ",":
+            if int(num) % 2 == 0:
+                count += 1
+            num = ""
+        else:
+            num += data[i]
+
+    # Check the last number
+    if num:
+        if int(num) % 2 == 0:
+            count += 1
+
+print(count)
